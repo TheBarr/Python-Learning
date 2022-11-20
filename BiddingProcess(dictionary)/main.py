2a591ae6e39eb863b3e8375ab08d94f890a10be2
Bartek <64077991+TheBarr@users.noreply.github.com>
2022-11-20 01:59:28 +0100

    Add files via upload

---
+++ BiddingProcess(dictionary)/main.py
@@ -0,0 +1,24 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+from art import logo
+print(logo)
+values = {}
+repeat = True
+while repeat:
+    print("Welcom to the secred auaction program.")
+    name = input("What's your name?: ")
+    bid = input("What's your bid?: ")
+    again = input("Is there any other bides? (yes/no): ").lower()
+    values[name] = bid
+    if again == "no":
+        repeat = False
+        highest_bid = 0
+        winner = ""
+        for name in values:
+            new_bid = int(values[name])
+            if new_bid > highest_bid:
+                highest_bid = new_bid
+                winner = name
+        print(f"The winnes is {winner} with a bid of ${highest_bid} bid")
+    elif again == "yes":
+        clear()
