fbacec18a67d7e1c00f307091afa973e13bfeb63
Bartek <64077991+TheBarr@users.noreply.github.com>
2023-05-20 01:05:37 +0200

    Update main.py

--- BiddingProcess(dictionary)/main.py
+++ BiddingProcess(dictionary)/main.py
@@ -5,7 +5,7 @@ print(logo)
 values = {}
 repeat = True
 while repeat:
-    print("Welcom to the secred auaction program.")
+    print("Welcme to the secred auaction program.")
     name = input("What's your name?: ")
     bid = input("What's your bid?: ")
     again = input("Are there any other bids? (yes/no): ").lower()
